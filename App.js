a90d9cfa0ba489a1b3e7b86a41f4ced7757f8cd0
wagner gomes <wagnergomes2132@gmail.com>
2024-10-09 19:45:30 -0300

    Primeiro passo

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.hello}> Hello Word!</Text>
+      <Text style={styles.emoji}> 😀 </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello:{
+    color: "brown",
+    fontWeight:"bold",
+    fontSize: 40,
+  },
+  emoji:{
+  fontSize: 40,
+  }
+ 
 });
